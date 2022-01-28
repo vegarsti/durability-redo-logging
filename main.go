be907918a3a3694984c6e9e51f6654be0c40002b
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2022-01-28 11:09:19 +0100

    Improve error handling

--- main.go
+++ main.go
@@ -8,9 +8,35 @@ import (
 	"time"
 )
 
+// Command is a struct used to deserialize a command from the log.
+// It may be a SetCommand or a DeleteCommand.
+// Since these use different keys, we can marshal into this catch-all
+// struct and check which struct field was populated.
 type Command struct {
 	Set    []string
 	Delete string
+}
+
+func (c Command) IsSetCommand() bool {
+	return len(c.Set) == 2
+}
+
+func (c Command) IsDeleteCommand() bool {
+	return len(c.Set) == 0
+}
+
+func (c Command) SetCommand() SetCommand {
+	if !c.IsSetCommand() {
+		panic("expected to be SetCommand")
+	}
+	return SetCommand{key: c.Set[0], value: c.Set[1]}
+}
+
+func (c Command) DeleteCommand() DeleteCommand {
+	if !c.IsDeleteCommand() {
+		panic("expected to be DeleteCommand")
+	}
+	return DeleteCommand{key: c.Delete}
 }
 
 type SetCommand struct {
@@ -18,6 +44,8 @@ type SetCommand struct {
 	value string
 }
 
+// Marshal the SetCommand into the JSON format used in the post.
+// Hand-writing the JSON isn't idiomatic, but it works.
 func (sc SetCommand) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`{"Set":["%s","%s"]}`, sc.key, sc.value)), nil
 }
@@ -26,6 +54,8 @@ type DeleteCommand struct {
 	key string
 }
 
+// Marshal the DeleteCommand into the JSON format used in the post.
+// Hand-writing the JSON isn't idiomatic, but it works.
 func (dc DeleteCommand) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`{"Delete":"%s"}`, dc.key)), nil
 }
@@ -37,72 +67,90 @@ type DB struct {
 }
 
 func NewDB(filename string) (*DB, error) {
-	// Open file for append writes, create file if it doesn't exist
+	// Open file for appends (creates file if it doesn't exist)
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("open: %w", err)
 	}
-	f.Sync()
+	if err := f.Sync(); err != nil {
+		return nil, fmt.Errorf("sync: %w", err)
+	}
 	db := &DB{
 		log:      f,
 		memtable: make(map[string]string),
 		filename: filename,
 	}
-	db.replayLog()
+	if err := db.replayLog(); err != nil {
+		return nil, fmt.Errorf("replayLog: %w", err)
+	}
 	return db, nil
 }
 
-func (d *DB) get(k string) (string, error) {
-	return d.memtable[k], nil
+func (d *DB) get(k string) string {
+	return d.memtable[k]
 }
 
-func (d *DB) set(k string, v string) {
+func (d *DB) set(k string, v string) error {
 	command := SetCommand{
 		key:   k,
 		value: v,
 	}
-	d.applySetCommand(command)
+	if err := d.applySetCommandToLog(command); err != nil {
+		return fmt.Errorf("applySetCommandToLog: %w", err)
+	}
 	d.applySetCommandToMemtable(command)
+	return nil
 }
 
-func (d *DB) delete(k string) {
-	command := DeleteCommand{
-		key: k,
+func (d *DB) delete(k string) error {
+	command := DeleteCommand{key: k}
+	if err := d.applyDeleteCommandToLog(command); err != nil {
+		return fmt.Errorf("applyDeleteCommandToLog: %w", err)
 	}
-	d.applyDeleteCommand(command)
 	d.applyDeleteCommandToMemtable(command)
+	return nil
 }
 
-func (d *DB) applySetCommand(command SetCommand) error {
+func (d *DB) applySetCommandToLog(command SetCommand) error {
 	bs, err := json.Marshal(command)
 	if err != nil {
 		return fmt.Errorf("marshal: %w", err)
 	}
-	d.log.Write(bs)
-	d.log.Write([]byte("\n"))
-	d.log.Sync()
+	if _, err := d.log.Write(bs); err != nil {
+		return fmt.Errorf("write command: %w", err)
+	}
+	if _, err := d.log.Write([]byte("\n")); err != nil {
+		return fmt.Errorf("write newline: %w", err)
+	}
+	if err := d.log.Sync(); err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
 	return nil
 }
 
-func (d *DB) applyDeleteCommand(command DeleteCommand) error {
+func (d *DB) applyDeleteCommandToLog(command DeleteCommand) error {
 	bs, err := json.Marshal(command)
 	if err != nil {
 		return fmt.Errorf("marshal: %w", err)
 	}
-	d.log.Write(bs)
-	d.log.Write([]byte("\n"))
-	d.log.Sync()
+	if _, err := d.log.Write(bs); err != nil {
+		return fmt.Errorf("write command: %w", err)
+	}
+	if _, err := d.log.Write([]byte("\n")); err != nil {
+		return fmt.Errorf("write newline: %w", err)
+	}
+	if err := d.log.Sync(); err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
 	return nil
 }
 
-func (d *DB) applySetCommandToMemtable(command SetCommand) error {
+func (d *DB) applySetCommandToMemtable(command SetCommand) {
 	d.memtable[command.key] = command.value
-	return nil
 }
 
-func (d *DB) applyDeleteCommandToMemtable(command DeleteCommand) error {
+func (d *DB) applyDeleteCommandToMemtable(command DeleteCommand) {
 	delete(d.memtable, command.key)
-	return nil
 }
 
 func (d *DB) replayLog() error {
@@ -114,14 +162,14 @@ func (d *DB) replayLog() error {
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		var cmd Command
-		json.Unmarshal(scanner.Bytes(), &cmd)
-		if len(cmd.Set) == 2 { // is Set command
-			k := cmd.Set[0]
-			v := cmd.Set[1]
-			d.applySetCommandToMemtable(SetCommand{key: k, value: v})
-		} else { // is Delete
-			k := cmd.Delete
-			d.applyDeleteCommandToMemtable(DeleteCommand{key: k})
+		if err := json.Unmarshal(scanner.Bytes(), &cmd); err != nil {
+			return fmt.Errorf("unmarshal: %w", err)
+		}
+		if cmd.IsSetCommand() {
+			d.applySetCommandToMemtable(cmd.SetCommand())
+		}
+		if cmd.IsDeleteCommand() {
+			d.applyDeleteCommandToMemtable(cmd.DeleteCommand())
 		}
 	}
 	return nil
